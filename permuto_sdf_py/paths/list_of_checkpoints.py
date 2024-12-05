cf1f79db5f8ad99fd40a5cc9ebb492fa14dd47bf
WJakubowska <nika99_11@o2.pl>
2024-12-05 12:10:01 +0000

    change paths

--- permuto_sdf_py/paths/list_of_checkpoints.py
+++ permuto_sdf_py/paths/list_of_checkpoints.py
@@ -4,21 +4,21 @@ ckpts={
 
     #DTU
     "dtu_with_mask_True":{
-        "dtu_scan24": "serial_train/full_dtu_dtu_scan24_with_mask_True_robo3/200000",
-        "dtu_scan37": "serial_train/full_dtu_dtu_scan37_with_mask_True_robo3/200000",
-        "dtu_scan40": "serial_train/full_dtu_dtu_scan40_with_mask_True_robo3/200000",
-        "dtu_scan55": "serial_train/full_dtu_dtu_scan55_with_mask_True_robo3/200000",
-        "dtu_scan63": "serial_train/full_dtu_dtu_scan63_with_mask_True_robo3/200000",
-        "dtu_scan65": "serial_train/full_dtu_dtu_scan65_with_mask_True_robo3/200000",
-        "dtu_scan69": "serial_train/full_dtu_dtu_scan69_with_mask_True_robo3/200000",
+        # "dtu_scan24": "serial_train/full_dtu_dtu_scan24_with_mask_True_robo3/200000",
+        # "dtu_scan37": "serial_train/full_dtu_dtu_scan37_with_mask_True_robo3/200000",
+        # "dtu_scan40": "serial_train/full_dtu_dtu_scan40_with_mask_True_robo3/200000",
+        # "dtu_scan55": "serial_train/full_dtu_dtu_scan55_with_mask_True_robo3/200000",
+        # "dtu_scan63": "serial_train/full_dtu_dtu_scan63_with_mask_True_robo3/200000",
+        # "dtu_scan65": "serial_train/full_dtu_dtu_scan65_with_mask_True_robo3/200000",
+        # "dtu_scan69": "serial_train/full_dtu_dtu_scan69_with_mask_True_robo3/200000",
         "dtu_scan83": "serial_train/full_dtu_dtu_scan83_with_mask_True_robo3/200000",
-        "dtu_scan97": "serial_train/full_dtu_dtu_scan97_with_mask_True_robo3/200000",
-        "dtu_scan105": "serial_train/full_dtu_dtu_scan105_with_mask_True_robo3/200000",
-        "dtu_scan106": "serial_train/full_dtu_dtu_scan106_with_mask_True_robo3/200000",
-        "dtu_scan110": "serial_train/full_dtu_dtu_scan110_with_mask_True_robo3/200000",
-        "dtu_scan114": "serial_train/full_dtu_dtu_scan114_with_mask_True_robo3/200000",
-        "dtu_scan118": "serial_train/full_dtu_dtu_scan118_with_mask_True_robo3/200000",
-        "dtu_scan122": "serial_train/full_dtu_dtu_scan122_with_mask_True_robo3/200000"
+        # "dtu_scan97": "serial_train/full_dtu_dtu_scan97_with_mask_True_robo3/200000",
+        # "dtu_scan105": "serial_train/full_dtu_dtu_scan105_with_mask_True_robo3/200000",
+        # "dtu_scan106": "serial_train/full_dtu_dtu_scan106_with_mask_True_robo3/200000",
+        # "dtu_scan110": "serial_train/full_dtu_dtu_scan110_with_mask_True_robo3/200000",
+        # "dtu_scan114": "serial_train/full_dtu_dtu_scan114_with_mask_True_robo3/200000",
+        # "dtu_scan118": "serial_train/full_dtu_dtu_scan118_with_mask_True_robo3/200000",
+        # "dtu_scan122": "serial_train/full_dtu_dtu_scan122_with_mask_True_robo3/200000"
     },
 
     "dtu_with_mask_False":{
@@ -46,7 +46,7 @@ ckpts={
         # "hotdog": "/workspace/permuto_sdf/checkpoints/permuto_sdf_hotdog_default/200000",
         # "materials": "/workspace/permuto_sdf/checkpoints/permuto_sdf_materials_default/200000",
         # "mic": "/workspace/permuto_sdf/checkpoints/permuto_sdf_mic_default/200000",
-        # "ship": "/workspace/permuto_sdf/checkpoints/permuto_sdf_ship_default/200000"
+        # "ship": "/workspace/permuto_sdf/checkpoints/permuto_sdf_ship_default/200000",
     }
 
 
